e7231ad62190b30fd507b7e98aea6eb592d7d297
malonhiro <45304762+malonhiro@users.noreply.github.com>
2024-01-24 05:53:08 +0000

    fix

--- trivy-docker-image-scan.sh
+++ trivy-docker-image-scan.sh
@@ -1,4 +1,4 @@
-dockerImageName=$(awk 'NR==1 {print $2}' DOckerfile)
+dockerImageName=$(awk 'NR==1 {print $2}' Dockerfile)
 echo $dockerImageName
 
 docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.17.2 -q image --exit 0 --severiry HIGH --light $dockerImageName
